f0309b5d1a25ce927d50ab86ad50af6bfab510f8
gobanoff <a.kliosovas@yahoo.com>
2024-02-20 20:23:47 +0200

    nnn

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
+use Faker\Factory as  Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +16,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $faker = Faker::create('fr_FR');
+        $mastersCount = 20;
+        foreach (range(1, 20) as $_) {
+            DB::table('masters')->insert([
+                'name' => $faker->firstName(),
+                'surname' => $faker->lastName(),
+            ]);
+        }
+
+        $outfits = ['Jacket','Shorts','Shirts','Coat','Pantes','Hat','Gloves','Skirt','Jeans'];
+        foreach (range(1, 200) as $_) {
+            DB::table('outfits')->insert([
+                'type' => $outfits[rand(0,count($outfits)-1)],
+                'color' => $faker->safeColorName(),
+                'size' => rand(5,22),
+                'about' => $faker->paragraph(5),
+                'master_id' => rand(1, $mastersCount)
+
+            ]);
+        }
+
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
