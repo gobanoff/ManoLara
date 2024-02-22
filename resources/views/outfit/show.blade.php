48a2811a86474c2c4aa524d2d0423026ea9bd3ef
gobanoff <a.kliosovas@yahoo.com>
2024-02-22 17:08:43 +0200

    Num

--- resources/views/outfit/show.blade.php
+++ resources/views/outfit/show.blade.php
@@ -3,30 +3,34 @@
 
 <div class="container">
    <div row justify-content-center>
-      <div class="col-md-8">
-         <div class="card">
+        <div class="col-md-8">
+            <div class="card">
 
-    <div class="card-header">{{$outfit->type}}</div>
+                <div class="card-header">{{$outfit->type}}</div>
 
-         <div class="card-body"> 
-           
-            
-           
-           
+                    <div class="card-body"> 
          
-               <div class="list">
-            <span>  color: <b> {{$outfit->color}} </b> size: <b>{{$outfit->size}} </b></span>
-            <p> {{$outfit->getMaster->name}} {{$outfit->getMaster->surname}}</p>
-            
-             </div>
-           
-      
+                      <div class="list1">
+                         <div class="show">  Color : <b class="show2"> {{$outfit->color}} </b></div>
+                         <div class="list3"style="background:{{$outfit->color}} ;"></div>
+                         <div class="show">  Size : <b class="show1">{{$outfit->size}} </b></div>
+                         <div class="show"> {{$outfit->type}} Master : <b class="show1">{{$outfit->getMaster->name}} {{$outfit->getMaster->surname}}</b></div>
+                      </div>
+
+                      <div class="list2">
+                        {{$outfit->about}}
+                      </div>
+                      <a href="{{route('outfit.edit',$outfit)}}"class="btn btn-outline-primary"> EDIT</a>
+
+
+
+
+
+                    </div>  
+                </div>
+            </div>
         </div>
-     
-   
-         </div>
-      </div>
-   </div>
+    </div>
 </div>
 
 @endsection
